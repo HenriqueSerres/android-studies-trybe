9e3735c3a0ee3c38e2ab08e30e03849becda5e76
Henrique Serres <henriqueserres@gmail.com>
2024-03-01 17:31:25 -0300

    lessons on first class

---
+++ src/main/kotlin/com/betrybe/aula01/App.kt
@@ -0,0 +1,10 @@
+package com.betrybe.aula01
+
+fun main() {
+    print("Digite seu nome: ")
+    var nome: String = readln()
+    println("Digite sua idade: ")
+    var idade: String = readln()
+
+    println("Hello $nome, $idade years old!")
+}
